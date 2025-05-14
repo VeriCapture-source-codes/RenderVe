530eb33cea043fbea6176cc6b25f226d17d566fc
Azeez muhammed <muhammedazeez09@gmail.com>
2025-05-14 20:05:33 +0100

    Added render.com/api/v1

--- FRONT-END/src/utils/api.js
+++ FRONT-END/src/utils/api.js
@@ -1,23 +1,28 @@
-const baseUrl = "https://renderve.onrender.com";
+const baseUrl = "https://renderve.onrender.com/api/v1";
 // const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
-  //import.meta.env.MODE === 'development'
-    //? 'http://localhost:5000/api/v1/'
-    //: 'https://www.vericapture.com.ng/api/v1/';
+//import.meta.env.MODE === 'development'
+//? 'http://localhost:5000/api/v1/'
+//: 'https://www.vericapture.com.ng/api/v1/';
 
-export async function apiRequest({ method = 'GET', route, body = null, formData = null }) {
+export async function apiRequest({
+  method = "GET",
+  route,
+  body = null,
+  formData = null,
+}) {
   try {
     const options = {
       method: method.toUpperCase(),
-      credentials: 'include', // for sending cookies
+      credentials: "include", // for sending cookies
       headers: {},
     };
 
     if (formData) {
       options.body = formData;
     } else if (body) {
-      options.headers['Content-Type'] = 'application/json';
-      options.headers['Accept'] = 'application/json';
+      options.headers["Content-Type"] = "application/json";
+      options.headers["Accept"] = "application/json";
       options.body = JSON.stringify(body);
     }
 
@@ -26,14 +31,15 @@ export async function apiRequest({ method = 'GET', route, body = null, formData 
 
     return {
       success: response.ok,
-      message: data.message || (response.ok ? 'Request successful' : 'Request failed'),
+      message:
+        data.message || (response.ok ? "Request successful" : "Request failed"),
       data: data || null,
       statusCode: response.status,
     };
   } catch (error) {
     return {
       success: false,
-      message: error.message || 'Something went wrong',
+      message: error.message || "Something went wrong",
       data: null,
       statusCode: null,
     };

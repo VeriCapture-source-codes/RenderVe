904abde94b8d84beec2d0fa44e8aeda1e99bd389
Azeez muhammed <muhammedazeez09@gmail.com>
2025-05-15 05:27:40 +0100

    change sideMenu

--- FRONT-END/src/Components/SideMenu.jsx
+++ FRONT-END/src/Components/SideMenu.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import { Nav } from 'react-bootstrap';
-import './SidebarMenu.css'; 
+import React from "react";
+import { Nav } from "react-bootstrap";
+import "./SideMenu.css";
 
 const SidebarMenu = () => {
   return (
     <div className="sidebar-container">
       <Nav defaultActiveKey="/home" className="sidebar-nav">
         <Nav.Link href="#" className="sidebar-link active">
-          <i className="bi bi-house" /><span>Home</span>
+          <i className="bi bi-house" />
+          <span>Home</span>
         </Nav.Link>
         <Nav.Link href="#" className="sidebar-link">
-          <i className="bi bi-compass" /><span>Explore</span>
+          <i className="bi bi-compass" />
+          <span>Explore</span>
         </Nav.Link>
         <Nav.Link href="#" className="sidebar-link">
-          <i className="bi bi-bell" /><span>Notification</span>
+          <i className="bi bi-bell" />
+          <span>Notification</span>
         </Nav.Link>
         <Nav.Link href="#" className="sidebar-link">
-          <i className="bi bi-person" /><span>Profile</span>
+          <i className="bi bi-person" />
+          <span>Profile</span>
         </Nav.Link>
         <Nav.Link href="#" className="sidebar-link">
-          <i className="bi bi-gear" /><span>Settings</span>
+          <i className="bi bi-gear" />
+          <span>Settings</span>
         </Nav.Link>
       </Nav>
     </div>
